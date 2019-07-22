073e978e676b24a0798d44b612cade3e7d032ff1
Boling <chelsea.x.boling.-nd@disney.com>
2019-07-21 19:52:44 -0700

    feature/changed intro info

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Howdy! <br />I'm Chelsea.</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1phHT2qizLBstLjc5QpsmHZs2fCtnxH_S/view?usp=sharing" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1zj2c15fmij2usoVxy9d6lLeGOe48p95k/view?usp=sharing" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
@@ -29,7 +29,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I build and store tools.</h1>
+                          <h1>I build tools.</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://github.com/cmboling" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
